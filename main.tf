4ccb78940072fc2e5828ddf5fa1dfd9750738cdc
jLuisreal <jchilu15@gmail.com>
2022-03-04 10:46:06 -0500

    newkeys

--- main.tf
+++ main.tf
@@ -23,8 +23,8 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  access_key = "AWSAccessKeyId"
-  secret_key = "AWSSecretKey"
+  access_key ="${var.AWS_ACCESS_KEY}"
+  secret_key = "${var.AWS_SECRET_KEY}"
 }
 
 
