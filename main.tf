697cd857189700b9bf995866b036ab34cfcf4fbb
jLuisreal <jchilu15@gmail.com>
2022-03-06 14:37:48 -0500

    commit

--- main.tf
+++ main.tf
@@ -30,14 +30,18 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-04505e74c0741db8d"
+  ami                    = "ami-0c293f3f676ec4f90"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+               sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+               curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+               sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+               apt-cache policy docker-ce
+               sudo apt install docker-ce
+               docker run -d -p 3000:8080 jordanjlu/nginxsite:latest
               EOF
 }
 
