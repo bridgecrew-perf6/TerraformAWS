36875632a1b5613cc227fed9f43754e788a98bf2
jLuisReal <100860344+jLuisReal@users.noreply.github.com>
2022-03-06 14:39:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,6 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
-<<<<<<< HEAD
               #!/bin/bash
                sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
                curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -43,18 +42,6 @@ resource "aws_instance" "web" {
                sudo apt install docker-ce
                docker run -d -p 3000:8080 jordanjlu/nginxsite:latest
               EOF
-=======
-    #!/bin/bash
-    set -ex
-    sudo yum update -y
-    sudo amazon-linux-extras install docker -y
-    sudo service docker start
-    sudo usermod -a -G docker ec2-user
-    sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-    sudo docker run -d -p 3000:80
-  EOF
->>>>>>> 0fadfd5e81e6bf5677350245d856bb822dd3cb9d
 }
 
 resource "aws_security_group" "web-sg" {
