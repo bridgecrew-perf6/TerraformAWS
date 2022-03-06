674b8708583d93cf40af9bb4ed554f4270805bcc
jLuisReal <100860344+jLuisReal@users.noreply.github.com>
2022-03-06 14:45:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0c293f3f676ec4f90"
+  ami                    = "ami-0e472ba40eb589f49"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
