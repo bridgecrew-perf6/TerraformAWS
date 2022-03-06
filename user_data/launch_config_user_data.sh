7995c90531cad5667d9b48bc8f8dc9a6ac8f381d
jLuisReal <100860344+jLuisReal@users.noreply.github.com>
2022-03-06 18:30:07 -0500

    Update launch_config_user_data.sh

--- user_data/launch_config_user_data.sh
+++ user_data/launch_config_user_data.sh
@@ -5,4 +5,5 @@ sudo adduser ubuntu docker
 newgrp docker
 sudo snap disable docker
 sudo snap enable docker
-sudo docker run -d -p 49512:80 jordanjlu/nginxsite:latest #host port will be 49512
+sleep 30
+sudo docker run -d -p 49512:80 jordanjlu/nginxsite:latest 
