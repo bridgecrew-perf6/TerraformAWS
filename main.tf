840b365e5111420b9199cfa24a9a38da115bc7e7
jLuisReal <100860344+jLuisReal@users.noreply.github.com>
2022-03-06 21:06:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,6 +47,16 @@ resource "aws_security_group" "web-sg" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+resource "aws_security_group" "web-sg" {
+  # ... other configuration ...
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
 output "web-address" {
